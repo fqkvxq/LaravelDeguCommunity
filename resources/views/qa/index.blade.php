0ce1991e07d3963f5041f26c673317acbf7aea77
fqkvxq <takuyakawai32@gmail.com>
2020-03-24 22:03:03 +0900

    2カラムに変更

--- resources/views/qa/index.blade.php
+++ resources/views/qa/index.blade.php
@@ -22,9 +22,6 @@
     </div>
     @endif
     <div class="row">
-        <div class="col-md-2">
-            左カラム
-        </div>
         <div class="col-md-8">
             <div class="row">
                 <div class="col-md-12 mx-auto px-0">
@@ -33,40 +30,22 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <p>
-                                        <button
-                                            class="btn btn-lg btn-block btn-success"
-                                            type="button"
-                                            data-toggle="collapse"
-                                            data-target="#QuestionCollapse"
-                                            aria-expanded="false"
-                                            aria-controls="QuestionCollapse"
-                                        >
+                                        <button class="btn btn-lg btn-block btn-success" type="button"
+                                            data-toggle="collapse" data-target="#QuestionCollapse" aria-expanded="false"
+                                            aria-controls="QuestionCollapse">
                                             質問を追加する！
                                         </button>
                                     </p>
                                     <div class="collapse" id="QuestionCollapse">
                                         <form method="POST" action="/qa/addQuestion">
                                             {{ csrf_field() }}
-                                            <input
-                                                type="hidden"
-                                                name="answer_flg"
-                                                value="0"
-                                            />
+                                            <input type="hidden" name="answer_flg" value="0" />
                                             <div class="form-group">
-                                                <label for="QuestionFrom"
-                                                    >質問を入力してください。</label
-                                                >
-                                                <textarea
-                                                    class="form-control"
-                                                    id="QuestionFrom"
-                                                    name="question_text"
-                                                    rows="7"
-                                                ></textarea>
+                                                <label for="QuestionFrom">質問を入力してください。</label>
+                                                <textarea class="form-control" id="QuestionFrom" name="question_text"
+                                                    rows="7"></textarea>
                                             </div>
-                                            <button
-                                                type="submit"
-                                                class="btn btn-block btn-primary"
-                                            >
+                                            <button type="submit" class="btn btn-block btn-primary">
                                                 質問を投稿する！
                                             </button>
                                         </form>
@@ -77,8 +56,9 @@
                     </div>
                 </div>
             </div>
+            <!-- カード -->
             <div class="row p-1">
-                <div class="col-md-12 bg-white shadow-sm rounded-sm p-3">
+                <div class="col-md-12 bg-white shadow-sm rounded p-3">
                     <div class="row">
                         <div class="col-md-12 tag">
                             <span>回答のある質問</span>
@@ -100,9 +80,33 @@
                             </p>
                         </div>
                     </div>
-                    <span class="details d-block text-right"
-                        ><a href="#">>>続きを読む</a></span
-                    >
+                    <span class="details d-block text-right"><a href="#">>>続きを読む</a></span>
+                </div>
+            </div>
+            <div class="row p-1">
+                <div class="col-md-12 bg-white shadow-sm rounded p-3">
+                    <div class="row">
+                        <div class="col-md-12 tag">
+                            <span>回答のある質問</span>
+                            <span class="viewcount">閲覧数：333</span>
+                            <span class="new">新着</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 question">
+                            <h2>
+                                デグーが夜うるさいときにはどうすれば良いでしょうか？
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 answer">
+                            <p>
+                                回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答
+                            </p>
+                        </div>
+                    </div>
+                    <span class="details d-block text-right"><a href="#">>>続きを読む</a></span>
                 </div>
             </div>
             <div class="row p-1">
@@ -128,47 +132,15 @@
                             </p>
                         </div>
                     </div>
-                    <span class="details d-block text-right"
-                        ><a href="#">>>続きを読む</a></span
-                    >
-                </div>
-            </div>
-            <div class="row p-1">
-                <div class="col-md-12 bg-white shadow-sm rounded-sm p-3">
-                    <div class="row">
-                        <div class="col-md-12 tag">
-                            <span>回答のある質問</span>
-                            <span class="viewcount">閲覧数：333</span>
-                            <span class="new">新着</span>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12 question">
-                            <h2>
-                                デグーが夜うるさいときにはどうすれば良いでしょうか？
-                            </h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12 answer">
-                            <p>
-                                回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答回答
-                            </p>
-                        </div>
-                    </div>
-                    <span class="details d-block text-right"
-                        ><a href="#">>>続きを読む</a></span
-                    >
+                    <span class="details d-block text-right"><a href="#">>>続きを読む</a></span>
                 </div>
             </div>
         </div>
-        <div class="col-md-2">
+        <div class="col-md-4">
             <div class="row p-1">
                 <div class="col-md-12 bg-white shadow-sm rounded-sm p-3">
                     <h2 class="text-center">まだ回答されていない質問</h2>
-                    <small class="d-block text-center"
-                        >みんなが回答を待っています！回答してね！</small
-                    >
+                    <small class="d-block text-center">みんなが回答を待っています！回答してね！</small>
                     <ul>
                         @foreach($questions as $question)
                         <li>
