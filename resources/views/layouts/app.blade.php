71ba647c8b45072b69a5a545e16b54558fdc96a4
fqkvxq <takuyakawai32@gmail.com>
2020-03-29 01:06:25 +0900

    色の修正 #37

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                         {{ config('app.name', '') }}
                     </span>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                <button onfocus="this.blur();" class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
