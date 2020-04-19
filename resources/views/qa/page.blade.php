27bcb3f0bae11bf96e7c9187e8567ec70864a9ff
fqkvxq <takuyakawai32@gmail.com>
2020-04-20 01:56:31 +0900

    デザイン修正

--- resources/views/qa/page.blade.php
+++ resources/views/qa/page.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     @if (session('success'))
     <div class="row">
-        <div class="col-12">
-            <div class="alert alert-success">
-                {{ session("success") }}
+        <div class="col-12 px-0">
+            <div class="alert alert-rainbow mt-1 mb-0 rounded-0">
+                <span class="d-block text-center">{{ session("success") }}</span>
             </div>
         </div>
     </div>
