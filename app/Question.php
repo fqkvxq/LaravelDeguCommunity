224211b5bcc3de2eb16c47b26dc29a4abdc32217
fqkvxq <takuyakawai32@gmail.com>
2020-05-06 12:54:36 +0900

    #176 ・質問カテゴリーテーブルの追加 ・質問テーブルと質問カテゴリーテーブルのリレーション ・質問テーブルにカテゴリーIDカラムの追加 ・マイグレーションファイル名の微修正

--- app/Question.php
+++ app/Question.php
@@ -14,6 +14,11 @@ class Question extends Model
         return $this->hasMany('App\Answer');
     }
 
+    // 質問:カテゴリー = N:1
+    public function categories(){
+        return $this->belongsTo('App\Category');
+    }
+
     // 質問:ユーザー = N:1
     public function user(){
         return $this->belongsTo('App\User');
