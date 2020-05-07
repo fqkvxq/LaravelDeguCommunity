670fe14756d7f556362a1b9b43a0213d8d8382cb
fqkvxq <takuyakawai32@gmail.com>
2020-05-07 14:52:02 +0900

    #153 通知ボタン作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,18 +64,19 @@
                         {{ config('app.name', '') }}
                     </span>
                 </a>
+                {{-- 通知ボタン --}}
+                <span class="text-right ml-auto notify-button"><i class="fas fa-bell"></i></span>
+                {{-- /通知ボタン --}}
                 <button onfocus="this.blur();" class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         {{-- <li>
