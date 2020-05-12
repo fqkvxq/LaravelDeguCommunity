a83bdb0d85109222ef393aa24b8f8239261d29d9
Takuya Kawai <takuyakawai@MacBook12.local>
2020-05-13 02:08:07 +0900

    #208 QuestionsTableSeederの微修正

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -16,8 +16,8 @@ class QuestionsTableSeeder extends Seeder
         $faker = Faker\Factory::create('ja_JP');
         for($i=1;$i<=15;$i++){
             DB::table('questions')->insert([
-                'user_id' => 1,
-                'text' => Str::random(20),
+                'user_id' => $i,
+                'text' => $faker->sentence(10),
                 'answer_flg' => 0,
                 'title' => Str::random(10),
                 'category_id' => $faker->numberBetween(1,5),	
