6a7dd1e16c7c64b2e7b070b709800778f46fcec7
fqkvxq <takuyakawai32@gmail.com>
2020-06-07 10:47:04 +0900

    サンプルvueファイル（navbarの一部）追加

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-light bg-white text-black shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <span class="appname">
+                    DeguCommunity
+                </span>
+            </a>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        async mounted() {
+            console.log('Component mounted.');
+            const ret = await window.axios.get("/api/sample")
+            console.log(ret.data)
+            this.name = ret.data.name;
+        },
+        data() {
+            return {
+                name: window.data.name,
+            };
+        }
+    }
+</script>
