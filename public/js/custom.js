fd6e16a5104879042f5c1a635678000f2a0f0de0
fqkvxq <takuyakawai32@gmail.com>
2020-06-07 10:48:55 +0900

    js変更

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,54 +1,100 @@
-// ====================================================
-// 画像アップロードしたときにファイル名を表示させる
-// ====================================================
-$(function() {
-    $('input[type=file]').after('<span></span>');
-  
-    // アップロードするファイルを選択
-    $('input[type=file]').change(function() {
-      var file = $(this).prop('files')[0];
-      console.log(file);
-      $('.custom-file-label').html(file.name+"("+file.size+"byte"+")");
-    });
-  });
-  
-/** 
- * ファイルサイズの単位
- * @param {int} file_size 
- * @return {string}
- */
-function getFiseSize(file_size)
-{
-var str;
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */,
+/* 1 */
+/*!**************************************!*\
+  !*** multi ./resources/js/custom.js ***!
+  \**************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-// 単位
-var unit = ['byte', 'KB', 'MB', 'GB', 'TB'];
+!(function webpackMissingModule() { var e = new Error("Cannot find module '/home/vagrant/data/LaravelDeguCommunity/resources/js/custom.js'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
 
-for (var i = 0; i < unit.length; i++) {
-if (file_size < 1024 || i == unit.length - 1) {
-if (i == 0) {
-    // カンマ付与
-    var integer = file_size.toString().replace(/([0-9]{1,3})(?=(?:[0-9]{3})+$)/g, '$1,');
-    str = integer +  unit[ i ];
-} else {
-    // 小数点第2位は切り捨て
-    file_size = Math.floor(file_size * 100) / 100;
-    // 整数と小数に分割
-    var num = file_size.toString().split('.');
-    // カンマ付与
-    var integer = num[0].replace(/([0-9]{1,3})(?=(?:[0-9]{3})+$)/g, '$1,');
-    if (num[1]) {
-    file_size = integer + '.' + num[1];
-    }
-    str = file_size +  unit[ i ];
-}
-break;
-}
-file_size = file_size / 1024;
-}
 
-return str;
-}
-// ====================================================
-// 
-// ====================================================
+/***/ })
+/******/ ]);
